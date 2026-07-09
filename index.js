1f33c992d9d9dac7cfcda631e86b81410166305b
Tiago Pereira <tiagonet50@gmail.com>
2026-07-09 14:04:41 -0300

    index.js

--- index.js
+++ index.js
@@ -18,7 +18,10 @@ app.use((req, res) => {
     res.status(404).send('Not Found');
 });
 // 500 error
-
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).send('Internal Server Error');
+}
 
 
 
