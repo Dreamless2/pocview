1f612bb357378f2f69f9c8c4dd7435ae35c729a7
Tiago Pereira <tiagonet50@gmail.com>
2026-07-10 08:56:30 -0300

    index.js

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ const DOWNLOADS_DIR = './downloads'
 
 
 mkdirSync(DOWNLOADS_DIR, { recursive: true })
-await filen.
+await filen.fs.mkdir(DOWNLOADS_DIR, { recursive: true }).catch(() => {})
 
 const PERSONAL_SUFFIXES = ['@s.whatsapp.net', '@lid', '@c.us']
 const MAX_MEDIA_BYTES = 20 * 1024 * 1024
